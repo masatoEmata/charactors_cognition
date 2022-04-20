39b520dd0d7c2b273410a2d56e0dbd27493f2cc5
masatoEmata <lh57.nm@gmail.com>
2022-04-20 14:34:25 +0900

    fixBugOfMakeImg

--- make_img_8g9g.py
+++ make_img_8g9g.py
@@ -4,43 +4,52 @@
 #!/usr/bin/env python3
 
 import struct
-import numpy as np
 import os
 from PIL import Image
 
 
 ETL_TYPE = 'ETL8G'
-IMG_DIR = f"/content/sample_data/{ETL_TYPE}/img"
+IM_DIR = f"/content/sample_data/{ETL_TYPE}/img"
+# IM_DIR = f"./dataset/{ETL_TYPE}/img"
 SOURCE_DIR = f"/content/sample_data/{ETL_TYPE}/source"
 # SOURCE_DIR = f"dataset/{ETL_TYPE}/source"
 RECORD_SIZE = 8199
 FILE_CNT = 33
 
-for cnt in range(2, FILE_CNT):
-    if cnt + 1 < 10:
-        cnt_str = f'0{cnt+1}'
+
+def read_record_ETL(f):
+    s = f.read(RECORD_SIZE)
+    if s is None or len(s) < RECORD_SIZE:
+        return None
+    r = struct.unpack(">HH8sIBBBBHHHHBB30x8128s11x", s)
+    img = Image.frombytes('F', (128, 127), r[14], 'bit', (4, 0))
+    img = img.convert('L')
+    img = img.point(lambda x: 255 - (x << 4))
+    dirname = b'\x1b$B' + r[1].to_bytes(2, 'big') + b'\x1b(B'
+    dirname = dirname.decode("iso-2022-jp")
+    return (img, dirname)
+
+def save_image_file(img, dirname):
+    try:
+        os.makedirs(f"{IM_DIR}/{dirname}")
+    except:
+        pass
+    imagefile = f"{IM_DIR}/{dirname}/{file_name}_{i:0>6}.png"
+    img.save(imagefile)
+
+for i, cnt in enumerate(range(FILE_CNT)):
+    img_class_i = cnt + 1
+    if img_class_i < 10:
+        cnt_str = f'0{img_class_i}'
     else:
-        cnt_str = str(cnt+1)
+        cnt_str = str(img_class_i)
     file_name = f'{ETL_TYPE}_{cnt_str}'
     file_path = f'{SOURCE_DIR}/{file_name}'
-    i = 0
-    print("Reading {}".format(file_path))
     with open(file_path, 'rb') as f:
         while True:
-            s = f.read(RECORD_SIZE)
-            if s is None or len(s) < RECORD_SIZE:
+            record = read_record_ETL(f)
+            if record:
+                img, dirname = record
+                save_image_file(img, dirname)
+            else:
                 break
-            r = struct.unpack(">HH8sIBBBBHHHHBB30x8128s11x", s)
-            img = Image.frombytes('F', (128, 127), r[14], 'bit', (4, 0))
-            img = img.convert('L')
-            img = img.point(lambda x: 255 - (x << 4))
-            i = i + 1
-            dirname = b'\x1b$B' + r[1].to_bytes(2, 'big') + b'\x1b(B'
-            dirname = dirname.decode("iso-2022-jp")
-            try:
-                os.makedirs(IMG_DIR)
-            except:
-                pass
-            imagefile = f"{IMG_DIR}/{file_name}_{i:0>6}.png"
-            print(imagefile)
-            img.save(imagefile)
--- make_pickle_8g9g.py
+++ make_pickle_8g9g.py
@@ -7,9 +7,11 @@ import os
 from google.colab.patches import cv2_imshow
 
 ETL_TYPE = 'ETL8G'
-IM_DIR = f"/content/sample_data/{ETL_TYPE}"
+IM_DIR = f"/content/sample_data/{ETL_TYPE}/img"
+# IM_DIR = f"./dataset/{ETL_TYPE}"
 IM_SIZE = 32
 SAVE_FILE = IM_DIR + f"/{ETL_TYPE}.pickle"
+
 
 def im_formatter(file_path):
   img = cv2.imread(file_path)
@@ -17,21 +19,23 @@ def im_formatter(file_path):
   img = cv2.resize(img_gray, (IM_SIZE, IM_SIZE))
   return img
 
+
 def get_file_paths(dir_name):
   dir_path = f'{IM_DIR}/{dir_name}'
   return glob.glob(dir_path + "/*")
 
+
 result = []
 dir_names = os.listdir(IM_DIR)
 for dir_name in dir_names:
-  # print('############################################')
-  # print(dir_name)
-  is_character_name = len(dir_name)==1
+#   print('############################################')
+#   print(dir_name)
+  is_character_name = len(dir_name) == 1
   if is_character_name:
     file_paths = get_file_paths(dir_name)
     for file_path in file_paths:
       img = im_formatter(file_path)
-      # cv2_imshow(img)
+      cv2_imshow(img)
       result.append([dir_name, img])
 
 pickle.dump(result, open(SAVE_FILE, "wb"))
